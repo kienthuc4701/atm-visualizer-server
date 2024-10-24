33756337963e4b854618694c8071d9f3bd6202f3
daweirdboiz <kienthuc4701@gmail.com>
2024-10-24 22:30:14 +0700

    add jwt middleware for APIs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { checkBalance, login, withdraw } from "./routes";
+import { checkBalance, login, refreshToken, withdraw } from "./routes";
+import { jwt } from "hono/jwt";
 
 interface Env {
-    assets: KVNamespace;
-    JWT_SECRET: string;
+  assets: KVNamespace;
+  JWT_SECRET: string;
 }
 
-const app = new Hono<{Bindings: Env}>();
+const app = new Hono<{ Bindings: Env }>();
 app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use("/auth/*", (c, next) => {
+  const jwtMiddleware = jwt({
+    secret: c.env.JWT_SECRET,
+    cookie: {
+      key: "accessToken",
+      secret: c.env.JWT_SECRET,
+    },
+    alg: "HS256",
+  });
+  return jwtMiddleware(c, next);
+});
 
 app.post("/login", login);
-app.get("/check-balance", checkBalance)
-app.put("/withdraw", withdraw)
+app.get("/auth/balance", checkBalance);
+app.put("/auth/withdraw", withdraw);
+app.post("/auth/refresh-token", refreshToken);
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,7 @@
 import { Context } from "hono";
-import { getSignedCookie, setCookie, setSignedCookie } from "hono/cookie";
+import { getCookie, getSignedCookie, setSignedCookie } from "hono/cookie";
 import { HTTPException } from "hono/http-exception";
-import { sign, verify } from "hono/jwt";
-
-// Utility functions
-const getToken = (c: Context): string | null => {
-  const authHeader = c.req.header("Authorization");
-  return authHeader ? authHeader.split(" ")[1] : null;
-};
+import { sign, verify, jwt } from "hono/jwt";
 
 const generateToken = async (payload: any, secret: string): Promise<string> => {
   return sign(payload, secret, "HS256");
@@ -21,125 +15,131 @@ const verifyToken = async (token: string, secret: string): Promise<any> => {
   }
 };
 
-// Handlers
 export const login = async (c: Context) => {
-  const { cardNumber, pin } = await c.req.json();
+  try {
+    const { cardNumber, pin } = await c.req.json();
 
-  // Check if user already has a valid token
-  const existingToken = getToken(c);
-  if (existingToken) {
-    const decoded = await verifyToken(existingToken, c.env.JWT_SECRET);
-    if (decoded) {
-      return c.json({ message: "Already logged in" });
+    // Check if user already has a valid token
+    const existingToken = await getSignedCookie(
+      c,
+      c.env.JWT_SECRET,
+      "accessToken"
+    );
+    if (existingToken) {
+      const decoded = await verifyToken(existingToken, c.env.JWT_SECRET);
+      if (decoded) {
+        return c.json({ message: "Already logged in" });
+      }
     }
+
+    // Proceed with normal login process
+    const user = await c.env.assets.get(cardNumber, "json");
+    if (!user || user.pin !== pin) {
+      throw new HTTPException(401, { message: "Unauthorized" });
+    }
+
+    const payload = {
+      sub: cardNumber,
+      exp: Math.floor(Date.now() / 1000) + 60 * 5,
+    }; // Token expires in 10 minutes
+    const newAccessToken = await generateToken(payload, c.env.JWT_SECRET);
+    const refreshTokenPayload = {
+      ...payload,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7,
+    }; // Refresh token expires in 7 days
+    const newRefreshToken = await generateToken(
+      refreshTokenPayload,
+      c.env.JWT_SECRET
+    );
+
+    // Update refreshToken in the database
+    await c.env.assets.put(
+      cardNumber,
+      JSON.stringify({ ...user, refreshToken: newRefreshToken })
+    );
+
+    // Set access token in cookie
+    setSignedCookie(c, "accessToken", newAccessToken, c.env.JWT_SECRET, {
+      httpOnly: true,
+      path: "/",
+      sameSite: "strict",
+      secure: true,
+    });
+    return c.json({ message: "Login successful!" });
+  } catch (error: any) {
+    if (error.name === "JwtTokenExpired") {
+      throw new HTTPException(401, { message: "Unauthorized" });
+    }
+    throw new HTTPException(500, { message: "Internal Server Error" });
   }
-
-  // Proceed with normal login process
-  const user = await c.env.assets.get(cardNumber, "json");
-
-  if (!user || user.pin !== pin) {
-    throw new HTTPException(401, { message: "Invalid credentials" });
-  }
-
-  const payload = {
-    sub: cardNumber,
-    exp: Math.floor(Date.now() / 1000) + 60 * 10,
-  }; // Token expires in 10 minutes
-  const newAccessToken = await generateToken(payload, c.env.JWT_SECRET);
-  const refreshToken = await generateToken(
-    { ...payload, exp: payload.exp + 60 * 24 * 7 },
-    c.env.JWT_SECRET
-  );
-
-  await c.env.assets.put(cardNumber, JSON.stringify({ ...user, refreshToken }));
-
-  await setSignedCookie(c, "accessToken", newAccessToken, c.env.JWT_SECRET, {
-    httpOnly: true,
-    path: "/",
-    sameSite: "strict",
-    secure: true,
-  });
-  return c.text("Login successful!", 200);
 };
-
 export const checkBalance = async (c: Context) => {
-  const token = await getSignedCookie(c, c.env.JWT_SECRET, "accessToken");
-
-  if (!token) throw new HTTPException(401, { message: "Unauthorized" });
-
-  const decoded = await verifyToken(token, c.env.JWT_SECRET);
-  if (!decoded) throw new HTTPException(401, { message: "Invalid token" });
-
-  const cardNumber = decoded.sub;
-  const { balance } = await c.env.assets.get(cardNumber, "json");
-  return c.json({ balance });
+  try {
+    const payload = c.get("jwtPayload");
+    const cardNumber = payload.sub;
+    const { balance } = await c.env.assets.get(cardNumber, "json");
+    return c.json({ balance });
+  } catch (error: any) {
+    console.error(error.name);
+    throw new HTTPException(500, { message: "Internal Server Error" });
+  }
 };
-
 export const withdraw = async (c: Context) => {
   try {
-    const token = await getSignedCookie(c, c.env.JWT_SECRET, "accessToken");
-    if (!token)
-      throw new HTTPException(401, {
-        message: "Unauthorized",
-        cause: "Missing token",
-      });
+    const payload = c.get("jwtPayload");
 
-    const decoded = await verifyToken(token, c.env.JWT_SECRET);
-    if (!decoded) throw new HTTPException(401, { message: "Invalid token" });
+    const cardNumber = payload.sub;
+    
+    const { amount } = await c.req.json();
 
-    const { cardNumber, amount } = await c.req.json();
     const account = await c.env.assets.get(cardNumber, "json");
 
-    if (account.balance < amount) {
+    const remain = account.balance - amount;
+
+    if (remain < 0) {
       throw new HTTPException(400, {
         message: "Bad Request",
-        cause: "Insufficient balance",
       });
     }
 
     await c.env.assets.put(
       cardNumber,
-      { ...account, balance: account.balance - amount },
-      "json"
+      JSON.stringify({ ...account, balance: remain })
     );
+    
     return c.json({ message: "Withdrawal successful" });
-  } catch (error) {
+  } catch (error: any) {
+    console.error(error);
     throw new HTTPException(500, { message: "Internal Server Error" });
   }
 };
-
 export const refreshToken = async (c: Context) => {
-  const { cardNumber } = await c.req.json();
-  const storedRefreshToken = await c.env.assets.get(cardNumber, "json");
-
-  if (!storedRefreshToken) {
-    throw new HTTPException(401, { message: "RefreshToken is required" });
-  }
-
   try {
+    const { cardNumber } = await c.req.json();
+    const { refreshToken: storedRefreshToken } = await c.env.assets.get(
+      cardNumber,
+      "json"
+    );
+
+    if (!cardNumber || !storedRefreshToken)
+      throw new HTTPException(401, { message: "Unauthorized" });
+
     await verifyToken(storedRefreshToken, c.env.JWT_SECRET);
 
     const payload = {
       sub: cardNumber,
-      exp: Math.floor(Date.now() / 1000) + 60 * 10,
+      exp: Math.floor(Date.now() / 1000) + 60 * 1,
     }; // Token expires in 10 minutes
     const newAccessToken = await generateToken(payload, c.env.JWT_SECRET);
 
-    // Ensure you're not overwriting the stored refresh token if you don’t need to
-    const account = await c.env.assets.get(cardNumber, "json");
-    await c.env.assets.put(
-      cardNumber,
-      JSON.stringify({ ...account, refreshToken: storedRefreshToken })
-    );
+    await setSignedCookie(c, "accessToken", newAccessToken, c.env.JWT_SECRET);
 
-    return c.json({ accessToken: newAccessToken });
+    return c.json({ status: 200 });
   } catch (error: any) {
-    if (error.name === "TokenExpiredError") {
+    if (error.name === "JwtTokenExpired")
       throw new HTTPException(401, {
-        message: "Refresh token expired, please log in again",
+        message: "Unauthorized",
       });
-    } else {
-      throw new HTTPException(401, { message: "Invalid refresh token" });
-    }
+    throw new HTTPException(500, { message: "Internal Server Error" });
   }
 };
